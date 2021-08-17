925820607b7844b3d818368dfab0f00ac88692cb
lotemn <lotemn@T-P-LOTEMN-WW>
2021-08-17 18:31:08 +0300

    checking low pass vs downsampling

--- preprocessing/check_angles_accuracy_on_different_fps.py
+++ preprocessing/check_angles_accuracy_on_different_fps.py
@@ -27,7 +27,15 @@ def calc_angle(p1: Point, p2: Point, p3: Point) -> float: # p1 is RFHD, pt2 is C
     angle = np.arccos(np.dot(vector1, vector2) / (np.linalg.norm(vector1) * np.linalg.norm(vector2)))
     return float(np.degrees(angle))
 
-def calc_average_angle_per_second(vicon_csv_path: str) -> (List, List):
+def calc_average_angle_per_second_downsampling(vicon_csv_path: str) -> (List, List):
+    """
+    Calculate average angle per second by using 120 frames per second, and by using 30 frames per second (sampling
+    every 4th frame).
+
+    :param vicon_csv_path: Path to the vicon points csv.
+    :return: average_angle_per_second_30: List of average angle per second calculated based on 30 fps,
+    average_angle_per_second_120: List of average angle per second calculated based on 120 fps
+    """
     RFHD_index = 1
     C7_index = 4
     RSHO_index = 16
@@ -104,7 +112,96 @@ def calc_average_angle_per_second(vicon_csv_path: str) -> (List, List):
 
     return average_angle_per_second_30, average_angle_per_second_120
 
-def visualize(vicon_csv_path: str, average_angle_per_second_120: List, average_angle_per_second_30: List):
+def calc_average_angle_per_second_low_pass_filter(vicon_csv_path: str) -> (List, List):
+    """
+    Calculate average angle per second by using 120 frames per second, and by using 30 frames per second (by applying
+    low pass filter).
+
+    :param vicon_csv_path: Path to the vicon points csv.
+    :return: average_angle_per_second_30: List of average angle per second calculated based on 30 fps,
+    average_angle_per_second_120: List of average angle per second calculated based on 120 fps
+    """
+    RFHD_index = 1
+    C7_index = 4
+    RSHO_index = 16
+
+    reader = VICONReader(vicon_file_path=vicon_csv_path)
+    vicon_points = reader.get_points()
+
+    angles_120 = []
+    angles_30 = []
+    nans_counter = 0
+
+    # Calculate each angle per frame, 120 FPS
+    for frame in vicon_points.keys():
+        points = vicon_points[frame]
+        RFHD = points[RFHD_index]
+        C7 = points[C7_index]
+        RSHO = points[RSHO_index]
+
+        angle = calc_angle(RFHD, C7, RSHO)
+
+        if not math.isnan(angle):
+            angles_120.append(angle)
+        else:
+            nans_counter += 1
+
+    # Calculate each angle per frame, 30 FPS
+    nans_counter = 0
+
+    for i in range(0, len(vicon_points.keys())-1, 4):
+        angles = []
+
+        for j in range(4):
+            if i+j >= len(vicon_points.keys()):
+                break
+
+            frame = list(vicon_points.keys())[i+j]
+            points = vicon_points[frame]
+            RFHD = points[RFHD_index]
+            C7 = points[C7_index]
+            RSHO = points[RSHO_index]
+            angle = calc_angle(RFHD, C7, RSHO)
+
+            if not math.isnan(angle):
+                angles.append(angle)
+            else:
+                nans_counter += 1
+                break
+
+        if not math.isnan(np.mean(angles)):
+            angles_30.append(np.mean(angles))
+
+    #print("Average 120: " + str(np.mean(angles_120)))
+    #print("Average 30: " + str(np.mean(angles_30)))
+
+    # Calculate average angle per second, 120 FPS
+    average_angle_per_second_120 = []
+
+    for i in range(len(angles_120) // 120):
+        sub_list = []
+
+        for j in range(120):
+            sub_list.append(angles_120[i*120+j])
+
+        assert len(sub_list) == 120
+        average_angle_per_second_120.append(np.mean(sub_list))
+
+    # Calculate average angle per second, 30 FPS
+    average_angle_per_second_30 = []
+
+    for i in range(len(angles_30) // 30):
+        sub_list = []
+
+        for j in range(30):
+            sub_list.append(angles_30[i*30 + j])
+
+        assert len(sub_list) == 30
+        average_angle_per_second_30.append(np.mean(sub_list))
+
+    return average_angle_per_second_30, average_angle_per_second_120
+
+def visualize_angle(vicon_csv_path: str, average_angle_per_second_120: List, average_angle_per_second_30: List):
     RFHD_index = 1
     C7_index = 4
     RSHO_index = 16
@@ -233,15 +330,13 @@ def visualize(vicon_csv_path: str, average_angle_per_second_120: List, average_a
     end = time.time()
     print(end - start)
 
-def plot(average_angle_per_second_120: List, average_angle_per_second_30: List, position: str, sub_name: str):
-    if len(average_angle_per_second_30) != len(average_angle_per_second_120):
-        #print("oh no")
-        return
-
+def plot(average_angle_per_second_120: List, average_angle_per_second_30_downsampled: List,
+         average_angle_per_second_30_low_pass: List, position: str, sub_name: str):
     # Create average angle plot
-    x = list(range(1, len(average_angle_per_second_30)+1))
+    x = list(range(1, len(average_angle_per_second_30_downsampled)+1))
     sns.set_style("darkgrid")
-    plt.plot(x, average_angle_per_second_30, label="30 FPS")
+    plt.plot(x, average_angle_per_second_30_downsampled, label="30 FPS downsample")
+    plt.plot(x, average_angle_per_second_30_low_pass, label="30 FPS low pass")
     plt.plot(x, average_angle_per_second_120, label="120 FPS")
     plt.title("Average angle per second in 120 FPS and 30 FPS (" + position + ")")
     plt.xlabel("Second")
@@ -252,27 +347,32 @@ def plot(average_angle_per_second_120: List, average_angle_per_second_30: List, 
     if not os.path.isdir(save_path):
         os.makedirs(save_path)
 
-    #plt.savefig(save_path + position + ".png")
+    plt.savefig(save_path + position + ".png")
     plt.close()
 
     # Create diff plot
-    x = list(range(1, len(average_angle_per_second_30) + 1))
-    y = [np.abs(average_angle_per_second_30[i]-average_angle_per_second_120[i]) for i in range(len(average_angle_per_second_30))]
+    x = list(range(1, len(average_angle_per_second_30_downsampled) + 1))
+    y_downsampled = [np.abs(average_angle_per_second_30_downsampled[i]-average_angle_per_second_120[i]) for i in \
+         range(len(average_angle_per_second_30_downsampled))]
+    y_low_pass = [np.abs(average_angle_per_second_30_low_pass[i] - average_angle_per_second_120[i]) for i in \
+                     range(len(average_angle_per_second_30_low_pass))]
     sns.set_style("darkgrid")
-    plt.plot(x, y)
+    plt.plot(x, y_downsampled, label="downsample")
+    plt.plot(x, y_low_pass, label="low pass")
     plt.title("Difference in average angle per second in 120 FPS and 30 FPS (" + position + ")")
     plt.xlabel("Second")
     plt.ylabel("Difference")
+    plt.legend()
     save_path = 'plots/angles_diff/' + sub_name + "/"
 
     if not os.path.isdir(save_path):
         os.makedirs(save_path)
 
-    #plt.savefig(save_path + position + "_diff.png")
+    plt.savefig(save_path + position + "_diff.png")
     plt.close()
 
-    max_diff = max(y)
-    mean_diff = np.mean(y)
+    max_diff = max(y_downsampled)
+    mean_diff = np.mean(y_downsampled)
 
     return (max_diff, mean_diff)
 
@@ -280,7 +380,7 @@ def generate_plots_for_all():
     max_diff_list = []
     mean_diff_list = []
 
-    for root, dirs, files in os.walk("/media/lotemn/Transcend/Movement Sense Research/Vicon Validation Study"):
+    for root, dirs, files in os.walk("../data"):
         for file in files:
             if file.endswith(".csv"):
                 if 'Sub001' in file or 'Sub002' in file or 'Sub003' in file or 'Sub006' in file:
@@ -316,10 +416,17 @@ def generate_plots_for_all():
 
                 #print("Working on " + subject_name + ", " + subject_position)
                 path = root + "/" + file
-                average_angle_per_second_30, average_angle_per_second_120 = calc_average_angle_per_second(path)
+                average_angle_per_second_30_downsampled, average_angle_per_second_120 = calc_average_angle_per_second_downsampling(path)
+                average_angle_per_second_30_low_pass, average_angle_per_second_120 = calc_average_angle_per_second_low_pass_filter(path)
 
-                res = plot(average_angle_per_second_120=average_angle_per_second_120, average_angle_per_second_30=average_angle_per_second_30,
-                        position=subject_position, sub_name=subject_name)
+                try:
+                    res = plot(average_angle_per_second_120=average_angle_per_second_120,
+                               average_angle_per_second_30_downsampled=average_angle_per_second_30_downsampled,
+                               average_angle_per_second_30_low_pass=average_angle_per_second_30_low_pass,
+                               position=subject_position, sub_name=subject_name)
+                except:
+                    print(subject_name)
+                    res = None
 
                 if res is None:
                     continue
@@ -349,7 +456,7 @@ def generate_plots_for_all():
     plt.title("Max difference in average angle per second between 120 FPS and 30 FPS")
     plt.xlabel("Max angle difference")
     plt.ylabel("Sessions count")
-    #plt.savefig("max_diff_dist.png")
+    plt.savefig("max_diff_dist.png")
     plt.close()
 
     sns.set_style("darkgrid")
@@ -358,10 +465,10 @@ def generate_plots_for_all():
     plt.title("Mean difference in average angle per second between 120 FPS and 30 FPS")
     plt.xlabel("Mean angle difference")
     plt.ylabel("Sessions count")
-    #plt.savefig("mean_diff_dist.png")
+    plt.savefig("mean_diff_dist.png")
 
 if __name__ == "__main__":
-    vicon_csv_path = '../../Data/Sub004 Left Leg.csv'
+    vicon_csv_path = '../../Data/Sub004 Left.csv'
     #average_angle_per_second_30, average_angle_per_second_120 = calc_average_angle_per_second(vicon_csv_path)
 
     #visualize(vicon_csv_path=vicon_csv_path, average_angle_per_second_120=average_angle_per_second_120,
