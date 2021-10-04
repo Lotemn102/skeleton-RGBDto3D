a0420389eb7066748d18789ff249e3f95b98c8fd
Lotem Nadir <35609587+Lotemn102@users.noreply.github.com>
2021-10-04 17:14:42 +0300

    Delete vicon-to-age/learning3d directory

--- vicon-to-age/learning3d/data_utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .dataloaders import ModelNet40Data
-from .dataloaders import ClassificationData, RegistrationData, SegmentationData, FlowData, SceneflowDataset
-from .dataloaders import download_modelnet40, deg_to_rad, create_random_transform
-from .user_data import UserData
